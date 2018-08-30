2b3c20f82d11418da45fd8947e883a97fb7268a7
github username <github email address>
2018-08-30 10:06:34 -0400

    Done

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-$('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
+// $('#save_lincoln').click(()=>saveLincoln(parseInt($('#interval').val())))
 
-function saveLincoln(interval) {
-  if(isNaN(interval)) {
-    interval = 10000
+const button = document.getElementById('save_lincoln')
+
+const img = document.getElementById('foreground')
+
+function saveLincoln() {
+//   if(isNaN(interval)) {
+//     interval = 10000
+//   }
+// $('#foreground').fadeOut(interval);
+
+  let interval = parseInt(document.getElementById('interval').value)
+  
+
+  if (isNaN(interval)) {
+    interval = 100
   }
-  $('#foreground').fadeOut(interval);
+
+    var fadeEffect = setInterval(function () {
+        if (!img.style.opacity) {
+            img.style.opacity = 1;
+        }
+        if (img.style.opacity > 0) {
+            img.style.opacity -= 0.001;
+        } else {
+            clearInterval(fadeEffect);
+        }
+    }, interval / 1000);
+
+
 }
+
+button.addEventListener("click", saveLincoln)
